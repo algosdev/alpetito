0175f9d27796e3047943960da580328daf6e54a3
prince-algo <samandarofficial149@gmail.com>
2020-08-03 08:56:58 +0500

    Fixed some bugs

--- js/script.js
+++ js/script.js
@@ -1,42 +1,54 @@
 const topScroller = document.querySelector('.top');
 window.addEventListener('scroll', () => {
- let c = window.scrollY;
- if(c>800){
-   topScroller.classList.add('active');
- }
- else {
-   topScroller.classList.remove('active');
- }
+  let c = window.scrollY;
+  if (c > 800) {
+    topScroller.classList.add('active');
+  }
+  else {
+    topScroller.classList.remove('active');
+  }
 });
 var swiper = new Swiper('.swiper-container', {
   initialSlide: 2,
-   loop: true,
-   breakpoints: {
-      100: {
-        slidesPerView: 1,
-        spaceBetween: 10
-      },
-      500: {
-          slidesPerView: 2,
-          spaceBetween: 15
-        },
-      800: {
-          slidesPerView: 3,
-          spaceBetween: 30 
-        }
+  loop: true,
+  breakpoints: {
+    100: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 15
+    },
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
 
   },
-   pagination: {
-     el: '.swiper-pagination',
-     clickable: true
-   },
-   navigation: {
-     nextEl: '.swiper-button-next',
-     prevEl: '.swiper-button-prev',
-   },
- });
-window.onload = function() {
-    document.querySelector('#doc').style.visibility = 'visible';
-    document.body.style.position = 'relative';
-    document.querySelector('.load').style.display = 'none';
-      };
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+let first = (localStorage.getItem("first") === null) ? true : localStorage.getItem("first");
+if (first == true) {
+  setTimeout(() => {
+    if (first) {
+      window.onload = whenOnload();
+      localStorage.setItem('first', 'false');
+    }
+  }, 5300);
+}
+else {
+  whenOnload();
+}
+function whenOnload() {
+  console.log("Loaded");
+  document.querySelector(".preload-cont").style.display = "none";
+  document.querySelector("body").style.overflow = "visible";
+}
